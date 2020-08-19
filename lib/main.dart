8b1fb1cefbcf5f0262954843f8e1d476067c25dd
sarthak singh <sarthakmno@gmail.com>
2020-08-18 23:18:36 -0700

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,38 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-void main(){
-  runApp(MaterialApp(home:NavPage()));
+
+void main() {
+  runApp(MaterialApp(home: NavPage()));
 }
+
 class NavPage extends StatefulWidget {
   @override
-  _NavPageState createState() => _NabvageState();
+  _NavPageState createState() => _NavPageState();
 }
 
 class _NavPageState extends State<NavPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:Text("Curved Navigation")
+      backgroundColor: Colors.blue,
+      appBar: AppBar(title: Text("Curved Navigation")),
+      bottomNavigationBar: CurvedNavigationBar(
+        buttonBackgroundColor: Colors.white,
+        animationCurve: Curves.ease,
+        backgroundColor: Colors.blue,
+        color: Colors.white,
+        index: 0,
+
+        animationDuration: Duration(milliseconds: 500),
+        height: 50,
+        // backgroundColor: Colors.white,
+        items: [
+          Icon(Icons.home),
+          Icon(Icons.search),
+          Icon(Icons.shopping_basket),
+          Icon(Icons.person)
+        ],
+        onTap: (index) {},
       ),
     );
   }
